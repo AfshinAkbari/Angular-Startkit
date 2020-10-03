aa472a360c575021b5bb1746b718dafa441ef6f1
mojtaba-ramezani72 <mojiramezani72@gmail.com>
2020-10-03 14:53:49 +0330

    del: Removing additional import from 'public-api'

--- projects/security/src/public-api.ts
+++ projects/security/src/public-api.ts
@@ -2,12 +2,10 @@
  * Public API Surface of security
  */
 
-export * from './lib/modules/main/main.module';
-export * from './lib/modules/main/main.component';
-export * from './lib/modules/layout/vertical/vertical.module';
-export * from './lib/modules/layout/vertical/vertical.component';
-export * from './lib/modules/layout/horizontal/horizontal.module';
-export * from './lib/modules/layout/horizontal/horizontal.component';
 export * from './lib/security.service';
 export * from './lib/security.component';
 export * from './lib/security.module';
+
+//todo: error at build command!
+export * from './lib/modules/main/main.module';
+export * from './lib/modules/main/main.component';
