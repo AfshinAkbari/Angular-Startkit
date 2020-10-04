420a63633de27dfd479ca2d447e87299b8cdd589
mojtaba-ramezani72 <mojiramezani72@gmail.com>
2020-10-04 17:31:55 +0330

    del: Removing unnecessary files

--- projects/pdn-security/src/public-api.ts
+++ projects/pdn-security/src/public-api.ts
@@ -1,11 +1,2 @@
-/*
- * Public API Surface of security
- */
-
-// export * from './lib/pdn-security.service';
-// export * from './lib/pdn-security.component';
-// export * from './lib/pdn-security.module';
-
-// todo: error at build command!
 export * from './lib/modules/main/main.module';
 export * from './lib/modules/public/public.module';
